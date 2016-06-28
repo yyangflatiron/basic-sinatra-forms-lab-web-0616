4ca950cac4e48343b8ffed1af88038ce284f5f5c
yyangflatiron <yichen.yang@flatironschool.com>
2016-06-28 13:26:08 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,21 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  get '/newteam' do
+    erb :newteam
+  end
 
+  post '/team' do
+    erb :team
+  end
 
 end
+
+
+# <!-- Team name ('name')
+#     Coach ('coach')
+#     Point Guard ('pg')
+#     Shooting Guard ('sg')
+#     Power Forward ('pf')
+#     Small Forward ('sf')
+#     Center ('c') -->
